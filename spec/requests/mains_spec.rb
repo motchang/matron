f83ed4a187846e6ca54088db54a02d70e0889e8c
motchang <motchang@gmail.com>
2018-09-21 13:54:20 +0900

    Remove auto-generated specs

--- spec/requests/mains_spec.rb
+++ spec/requests/mains_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Mains", type: :request do
   describe "GET /mains" do
     it "works! (now write some real specs)" do
-      get mains_path
+      get root_path
       expect(response).to have_http_status(200)
     end
   end
